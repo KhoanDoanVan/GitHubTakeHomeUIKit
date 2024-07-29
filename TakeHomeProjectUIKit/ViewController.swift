fcb180e5755e7166d23d78e3943cb0cf23e853ed
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-07-29 18:00:03 +0700

    Initial Commit

---
+++ TakeHomeProjectUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeHomeProjectUIKit
+//
+//  Created by Đoàn Văn Khoan on 29/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
