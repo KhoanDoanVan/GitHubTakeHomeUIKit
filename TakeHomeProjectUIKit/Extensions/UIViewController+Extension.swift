0845d72d78b6ad78bdca7ac502069dd279014a4b
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-08-09 10:58:58 +0700

    progress view

--- TakeHomeProjectUIKit/Extensions/UIViewController+Extension.swift
+++ TakeHomeProjectUIKit/Extensions/UIViewController+Extension.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
 
 extension UIViewController {
    
@@ -16,6 +17,38 @@ extension UIViewController {
             alertViewController.modalPresentationStyle = .overFullScreen
             alertViewController.modalTransitionStyle = .crossDissolve
             self.present(alertViewController, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerView.alpha = 0.8
+        }
+        
+        /// Progress View
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
