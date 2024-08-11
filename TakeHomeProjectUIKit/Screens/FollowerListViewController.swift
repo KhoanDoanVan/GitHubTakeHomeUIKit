e4592c64a9a0a0efa8d54261b5a9f47667966c0c
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-08-11 12:16:16 +0700

    destination to UserInfoVS

--- TakeHomeProjectUIKit/Screens/FollowerListViewController.swift
+++ TakeHomeProjectUIKit/Screens/FollowerListViewController.swift
@@ -19,6 +19,7 @@ class FollowerListViewController: UIViewController {
     
     var page = 1
     var hasMoreFollowers = true
+    var isSearching = false
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section,Follower>!
@@ -129,18 +130,33 @@ extension FollowerListViewController: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    /// Tap on user
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filteredFollowers : followers
+        let follower = activeArray[indexPath.item]
+                
+        /// Navigation
+        let destinationViewController = UserInfoViewController()
+        destinationViewController.username = follower.login
+        let navController = UINavigationController(rootViewController: destinationViewController)
+        present(navController, animated: true)
+        
+    }
 }
 
 
 extension FollowerListViewController: UISearchResultsUpdating, UISearchBarDelegate {
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        isSearching = true
         filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
         updateData(on: filteredFollowers)
     }
     
     /// The method of UISearchBarDelegate
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
         updateData(on: followers)
     }
 }
